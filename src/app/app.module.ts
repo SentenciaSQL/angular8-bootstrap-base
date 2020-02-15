962283b93c832b6bdb50a2bcee3ec84c3b4b4df2
Andres Frias <afrias@eisdr.com>
2020-02-15 17:34:38 -0400

    añadir componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ButtonsComponent } from './components/components/buttons/buttons.component';
+import { CardsComponent } from './components/components/cards/cards.component';
+import { ColorsComponent } from './components/utilities/colors/colors.component';
+import { BordersComponent } from './components/utilities/borders/borders.component';
+import { AnimationsComponent } from './components/utilities/animations/animations.component';
+import { OtherComponent } from './components/utilities/other/other.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { ForgotPasswordComponent } from './components/pages/forgot-password/forgot-password.component';
+import { PageNotFoundComponent } from './components/pages/page-not-found/page-not-found.component';
+import { BlankComponent } from './components/pages/blank/blank.component';
+import { ChartsComponent } from './components/charts/charts.component';
+import { TablesComponent } from './components/tables/tables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    DashboardComponent,
+    ButtonsComponent,
+    CardsComponent,
+    ColorsComponent,
+    BordersComponent,
+    AnimationsComponent,
+    OtherComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    PageNotFoundComponent,
+    BlankComponent,
+    ChartsComponent,
+    TablesComponent
   ],
   imports: [
     BrowserModule,
